ceeb1f8cbaf4cae374936ecec5aed8600e925494
MercuryWitch <10678940+MercuryWitch@users.noreply.github.com>
2020-06-07 16:37:50 -0700

    Update RPiP.py

--- RPiP.py
+++ RPiP.py
@@ -1,7 +1,6 @@
 import discord
 import asyncio
 import requests
-from discord.ext import commands
 
 
 # The token is essentially the Bot's password for Discord, ensure it is kept safe.
@@ -10,35 +9,31 @@ TOKEN = 'YOUR TOKEN HERE'
 # The Dominions 5 server's ip-address channel id. Obtained by right clicking the channel and copying ID.
 CHANNEL_ID = 'YOUR CHANNEL ID HERE'
 
-description = '''RPiP in Python: Reports the IP of the device as a channel message on Discord.'''
-bot = commands.Bot(command_prefix='?', description=description)
+
+class RPiPClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # create the background task and run it in the background
+        self.bg_task = self.loop.create_task(self.report_ip_background_task())
+
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def report_ip_background_task(self):
+        wait self.wait_until_ready()
+        channel = self.get_channel(CHANNEL_ID)  # channel ID goes here as an integer
+        old_ip = requests.get('https://checkip.amazonaws.com').text.strip()
+
+        while not self.is_closed():
+            current_ip = requests.get('https://checkip.amazonaws.com').text.strip()
+            if old_ip != current_ip:
+                await channel.send(current_ip)
+            await asyncio.sleep(300)  # task runs every 300 seconds (5 minutes)
 
 
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-
-
-@bot.command()
-async def hello(ctx):
-    """Says world"""
-    await ctx.send("world")
-
-
-@bot.command()
-async def report_ip(self):
-    await self.wait_until_ready()
-    channel = self.get_channel(CHANNEL_ID)  # channel ID goes here as an integer
-    old_ip = requests.get('https://checkip.amazonaws.com').text.strip()
-
-    while not self.is_closed():
-        current_ip = requests.get('https://checkip.amazonaws.com').text.strip()
-        if old_ip != current_ip:
-            await channel.send(current_ip)
-        await asyncio.sleep(300)  # task runs every 300 seconds (5 minutes)
-
-
-bot.run(TOKEN)
+client = RPiPClient()
+client.run(TOKEN)
