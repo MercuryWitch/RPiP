1eaaa9a813f8722c563a6a204bfb000afe31fd1c
MercuryWitch <10678940+MercuryWitch@users.noreply.github.com>
2020-06-08 00:45:41 -0700

    Update RPiP.py
    Added a way to check the ip anytime by using the message command ?ip

--- RPiP.py
+++ RPiP.py
@@ -7,7 +7,7 @@ import requests
 TOKEN = 'YOUR TOKEN HERE'
 
 # The Dominions 5 server's ip-address channel id. Obtained by right clicking the channel and copying ID.
-CHANNEL_ID = 'YOUR CHANNEL ID HERE'
+CHANNEL_ID = 0  # YOUR CHANNEL ID HERE, it should be an integer
 
 
 class RPiPClient(discord.Client):
@@ -25,14 +25,23 @@ class RPiPClient(discord.Client):
 
     async def report_ip_background_task(self):
         await self.wait_until_ready()
-        channel = self.get_channel(CHANNEL_ID)  # channel ID goes here as an integer
+        channel = self.get_channel(CHANNEL_ID)
         old_ip = requests.get('https://checkip.amazonaws.com').text.strip()
-
+        await channel.send(old_ip)
+        
         while not self.is_closed():
             current_ip = requests.get('https://checkip.amazonaws.com').text.strip()
             if old_ip != current_ip:
                 await channel.send(current_ip)
             await asyncio.sleep(300)  # task runs every 300 seconds (5 minutes)
+    
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('?ip'):
+            await message.channel.send(requests.get('https://checkip.amazonaws.com').text.strip())
 
 
 client = RPiPClient()
